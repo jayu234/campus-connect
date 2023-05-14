0d0b15104daade5f9dc38fdfbb8fe4f38acb28ff
Jayu <jaivikgoswami780@gmail.com>
2023-05-14 18:17:52 +0530

    configured CORS in backend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cookieparser =require("cookie-parser");
 const cors = require("cors");
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND,
     credentials: true,
 }));
 app.use(express.json());
