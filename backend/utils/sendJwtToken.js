815bfa6643725a1f1da1e303fc2f5c998eaa0d82
Jayu <jaivikgoswami780@gmail.com>
2023-05-14 23:35:35 +0530

    authentication bug fixed

--- backend/utils/sendJwtToken.js
+++ backend/utils/sendJwtToken.js
@@ -6,6 +6,8 @@ const sendToken = (user, res, statusCode, message) => {
             Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        sameSite: 'none',
+        secure: true,
         path: '/'
     }
 
